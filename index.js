9e494e17fdcb8f3da9365d1a465a1dad9d0f6f24
Nicholas Orrell <mrorrell@gmail.com>
2021-05-16 18:33:49 -0400

    Removed prettier messages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['prettier', 'airbnb'],
-  plugins: ['prettier', 'import', 'only-warn'],
+  plugins: ['import', 'only-warn'],
   env: {
     es2021: true,
     node: true,
@@ -13,10 +13,6 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': [
-      'error',
-      { singleQuote: true, bracketSpacing: false },
-    ],
     'no-use-before-define': 0,
     'arrow-body-style': 0,
   },
